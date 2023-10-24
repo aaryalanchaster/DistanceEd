9ba485ea3028d9567daeb3e32d73cb89ccdfa93f
DivyakumarPatel <83291936+DivyakumarPatel@users.noreply.github.com>
2023-10-24 16:33:35 -0400

    Made changes for lab 5

--- myappF23/views.py
+++ myappF23/views.py
@@ -1,29 +1,57 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Category, Course
-
-# Create your views here.
 
 
 def index(request):
     category_list = Category.objects.all().order_by('id')[:10]
     course_list = Course.objects.order_by('-price')[:5]
-    context = {
-        'category_list': category_list,
-        'course_list': course_list,
-    }
-    return render(request, 'index.html', context)
+    response = HttpResponse(content_type='text/html')
+    response.write('<h1>List of Categories:</h1>')
+
+    if category_list:
+        response.write('<ul>')
+        for category in category_list:
+            response.write(f'<li>{category.name}</li>')
+        response.write('</ul>')
+    else:
+        response.write('<p>No categories found.</p>')
+        
+    response.write('<h1>Top 5 Most Expensive Courses:</h1>')
+
+    if course_list:
+        response.write('<ul>')
+        for course in course_list:
+            response.write(f'<li>{course.title} - Price: ${course.price}</li>')
+        response.write('</ul>')
+    else:
+        response.write('<p>No courses found.</p>')
+
+    return response
 
 
 def about(request):
-    return render(request, 'about.html')
+    response = HttpResponse(content_type='text/html')
+    response.write("<h1>About Us</h1>")
+    response.write(
+        "<p>This is a Distance Education Website! Search our Categories to find all available Courses.</p>")
+    return response
 
 
 def detail(request, category_no):
     category = get_object_or_404(Category, pk=category_no)
     courses = Course.objects.filter(categories=category)
-    context = {
-        'category': category,
-        'courses': courses,
-    }
-    return render(request, 'detail.html', context)
+
+    response = HttpResponse(content_type='text/html')
+    response.write(f'<h1>Category: {category.name}</h1>')
+
+    if courses:
+        response.write('<h2>Courses in this Category:</h2>')
+        response.write('<ul>')
+        for course in courses:
+            response.write(f'<li>{course.title} - Price: ${course.price}</li>')
+        response.write('</ul>')
+    else:
+        response.write('<p>No courses found in this category.</p>')
+
+    return response
