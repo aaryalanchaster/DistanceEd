d0b5b87e3f3aea3a0d39bad410e247806718490e
DivyakumarPatel <83291936+DivyakumarPatel@users.noreply.github.com>
2023-10-24 17:15:45 -0400

    Added Lab 6

--- myappF23/urls.py
+++ myappF23/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
     path('<int:category_no>/', views.detail, name='detail'),
+    path('instructor/<int:instructor_id>/',
+         views.instructor, name='instructor'),
 ]
--- myappF23/views.py
+++ myappF23/views.py
@@ -1,57 +1,40 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
-from .models import Category, Course
+from .models import Category, Course, Instructor
+
+# Create your views here.
 
 
 def index(request):
     category_list = Category.objects.all().order_by('id')[:10]
-    course_list = Course.objects.order_by('-price')[:5]
-    response = HttpResponse(content_type='text/html')
-    response.write('<h1>List of Categories:</h1>')
-
-    if category_list:
-        response.write('<ul>')
-        for category in category_list:
-            response.write(f'<li>{category.name}</li>')
-        response.write('</ul>')
-    else:
-        response.write('<p>No categories found.</p>')
-        
-    response.write('<h1>Top 5 Most Expensive Courses:</h1>')
-
-    if course_list:
-        response.write('<ul>')
-        for course in course_list:
-            response.write(f'<li>{course.title} - Price: ${course.price}</li>')
-        response.write('</ul>')
-    else:
-        response.write('<p>No courses found.</p>')
-
-    return response
+    return render(request, 'myappF23/index0.html', {'category_list': category_list})
 
 
 def about(request):
-    response = HttpResponse(content_type='text/html')
-    response.write("<h1>About Us</h1>")
-    response.write(
-        "<p>This is a Distance Education Website! Search our Categories to find all available Courses.</p>")
-    return response
+    return render(request, "myappF23/about0.html")
 
 
 def detail(request, category_no):
     category = get_object_or_404(Category, pk=category_no)
     courses = Course.objects.filter(categories=category)
 
-    response = HttpResponse(content_type='text/html')
-    response.write(f'<h1>Category: {category.name}</h1>')
+    context = {
+        'category': category,
+        'courses': courses,
+    }
 
-    if courses:
-        response.write('<h2>Courses in this Category:</h2>')
-        response.write('<ul>')
-        for course in courses:
-            response.write(f'<li>{course.title} - Price: ${course.price}</li>')
-        response.write('</ul>')
-    else:
-        response.write('<p>No courses found in this category.</p>')
+    return render(request, 'myappF23/detail0.html', context)
 
-    return response
+
+def instructor(request, instructor_id):
+    instructor = Instructor.objects.get(pk=instructor_id)
+    courses_taught = instructor.course_set.all()
+    students = instructor.students.all()
+
+    context = {
+        'instructor': instructor,
+        'courses_taught': courses_taught,
+        'students': students,
+    }
+
+    return render(request, 'myappF23/instructor0.html', context)
